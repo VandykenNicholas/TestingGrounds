24e67350827b35bd4275aabf910802c9ba57e42b
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-12-08 16:09:35 -0500

    testing again

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test 1</Text>
+      <Text>Test ------- 2</Text>
       <StatusBar style="auto" />
     </View>
   );
